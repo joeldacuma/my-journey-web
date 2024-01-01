db8828d1469891d0765bc2ad43208fed3c23400e
Joel Dacuma <joeldacuma1@gmail.com>
2024-01-01 11:56:45 +0800

    install ngx-env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  strapi: 'http://localhost:1337/api'
+  strapi: 'http://localhost:1337/api',
+  API_KEYS: import.meta.env['NG_APP_STRAPI_KEY']
 };
