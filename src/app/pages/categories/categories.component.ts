9785aa2cc758c3ba0b58e069d0209155e7dd0698
Joel Dacuma <joeldacuma1@gmail.com>
2024-01-15 08:21:20 +0800

    update in categories page

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { lastValueFrom, map } from "rxjs";
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
-
+  public loading = signal(false);
+  
+  constructor() {
+    this.loading();
+  }
 }
